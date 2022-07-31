43470606522cbfe8ffb386921526b144f6cd4bcd
여지원 <yooara0508@naver.com>
2022-08-01 01:10:05 +0900

    Chore : 프로젝트 세팅

---
+++ slow_delivery/src/main/java/be/shop/slow_delivery/SlowDeliveryApplication.java
@@ -0,0 +1,13 @@
+package be.shop.slow_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowDeliveryApplication.class, args);
+	}
+
+}
---
+++ slow_delivery/src/test/java/be/shop/slow_delivery/SlowDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package be.shop.slow_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
