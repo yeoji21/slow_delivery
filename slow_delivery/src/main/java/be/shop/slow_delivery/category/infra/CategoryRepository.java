92d94e8f9913342f98a74b5e363828d95c0c0053
이연희 <ydol2@naver.com>
2022-08-02 02:15:44 +0900

    [ADD] category entity

---
+++ slow_delivery/src/main/java/be/shop/slow_delivery/category/infra/CategoryRepository.java
@@ -0,0 +1,7 @@
+package be.shop.slow_delivery.category.infra;
+
+import be.shop.slow_delivery.category.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
