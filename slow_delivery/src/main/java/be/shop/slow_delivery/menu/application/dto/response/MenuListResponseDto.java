710d1325dc42376e1cd2243bfceaaa11e5669bdf
이연희 <ydol2@naver.com>
2022-08-02 02:35:39 +0900

    [ADD] menu - response dto

---
+++ slow_delivery/src/main/java/be/shop/slow_delivery/menu/application/dto/response/MenuListResponseDto.java
@@ -0,0 +1,18 @@
+package be.shop.slow_delivery.menu.application.dto.response;
+
+import be.shop.slow_delivery.menu.application.dto.MenuListDto;
+import be.shop.slow_delivery.shop.domain.Shop;
+import lombok.Data;
+
+@Data
+public class MenuListResponseDto {
+
+    private Long shopPK;
+
+    private MenuListDto menuListDto;
+
+    // 수정 필요 shop - menulist 받아오기
+//    public MenuListResponseDto(Shop shop){
+//        menuListDto = new MenuListDto(shop.getMenuList());
+//    }
+}
