7fc0905eb80a7da29bf50c599f115e58ddade197
여지원 <yooara0508@naver.com>
2022-08-17 15:36:38 +0900

    Feat : Option, OptionGroup 엔티티 추가

---
+++ slow_delivery/src/main/java/be/shop/slow_delivery/product/domain/Option.java
@@ -0,0 +1,40 @@
+package be.shop.slow_delivery.product.domain;
+
+
+import be.shop.slow_delivery.common.domain.BaseTimeEntity;
+import be.shop.slow_delivery.common.domain.Money;
+import com.mysema.commons.lang.Assert;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@EqualsAndHashCode(of = "id", callSuper = false)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Option extends BaseTimeEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "option_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Embedded
+    @Column(name = "price", nullable = false)
+    private Money price;
+
+    @Embedded
+    private StockInfo stockInfo;
+
+    @Builder
+    public Option(String name, Money price, Long stockId) {
+        Assert.hasText(name, "name");
+        Assert.notNull(price, "price");
+        Assert.notNull(stockId, "stockId");
+
+        this.name = name;
+        this.price = price;
+        this.stockInfo = new StockInfo(stockId);
+    }
+}
---
+++ slow_delivery/src/main/java/be/shop/slow_delivery/product/domain/OptionGroup.java
@@ -0,0 +1,34 @@
+package be.shop.slow_delivery.product.domain;
+
+import be.shop.slow_delivery.common.domain.Quantity;
+import com.mysema.commons.lang.Assert;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@EqualsAndHashCode(of = "id", callSuper = false)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class OptionGroup {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "option_group_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "max_select_count", nullable = false)
+    private Quantity maxSelectCount;
+
+    public OptionGroup(String name, Quantity maxSelectCount) {
+        Assert.hasText(name, "name");
+        Assert.notNull(maxSelectCount, "maxSelectCount");
+
+        this.name = name;
+        this.maxSelectCount = maxSelectCount;
+    }
+}
