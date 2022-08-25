b572858b5eafddb26a27c1cb60abafa8e4c9911b
여지원 <yooara0508@naver.com>
2022-08-25 18:00:00 +0900

    Feat : 상품 상세 정보 조회 API 개발 완료

--- slow_delivery/src/main/java/be/shop/slow_delivery/product/infra/ProductQueryDao.java
+++ slow_delivery/src/main/java/be/shop/slow_delivery/product/infra/ProductQueryDao.java
@@ -1,9 +1,24 @@
 package be.shop.slow_delivery.product.infra;
 
-import be.shop.slow_delivery.product.application.dto.ProductDetailInfo;
+import be.shop.slow_delivery.product.application.dto.*;
+import com.mysema.commons.lang.Assert;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static be.shop.slow_delivery.product.domain.QIngredient.ingredient;
+import static be.shop.slow_delivery.product.domain.QIngredientGroup.ingredientGroup;
+import static be.shop.slow_delivery.product.domain.QIngredientInGroup.ingredientInGroup;
+import static be.shop.slow_delivery.product.domain.QOption.option;
+import static be.shop.slow_delivery.product.domain.QOptionGroup.optionGroup;
+import static be.shop.slow_delivery.product.domain.QOptionInGroup.optionInGroup;
+import static be.shop.slow_delivery.product.domain.QProduct.product;
+import static be.shop.slow_delivery.product.domain.QProductIngredientGroup.productIngredientGroup;
+import static be.shop.slow_delivery.product.domain.QProductOptionGroup.productOptionGroup;
+import static com.querydsl.core.group.GroupBy.groupBy;
+import static com.querydsl.core.group.GroupBy.list;
 
 @RequiredArgsConstructor
 @Repository
@@ -11,6 +26,46 @@ public class ProductQueryDao {
     private final JPAQueryFactory queryFactory;
 
     public ProductDetailInfo findProductDetailInfo(long productId) {
-        return null;
+        ProductDetailInfo productDetailInfo = queryFactory
+                .select(new QProductDetailInfo(product.id, product.name, product.description,
+                        product.price, product.maxOrderQuantity, product.thumbnailFileId))
+                .from(product)
+                .where(product.id.eq(productId))
+                .fetchOne();
+        Assert.notNull(productDetailInfo, "productId " + productId + " is not found");
+
+        List<IngredientGroupInfo> ingredientGroups = queryFactory
+                .from(productIngredientGroup)
+                .leftJoin(productIngredientGroup.ingredientGroup, ingredientGroup)
+                .leftJoin(ingredientInGroup).on(ingredientInGroup.ingredientGroup.eq(ingredientGroup))
+                .leftJoin(ingredientInGroup.ingredient, ingredient)
+                .where(productIngredientGroup.product.id.eq(productId),
+                        ingredientInGroup.displayInfo.isDisplay.isTrue())
+                .orderBy(productIngredientGroup.displayOrder.displayOrder.asc(),
+                        ingredientInGroup.displayInfo.displayOrder.displayOrder.asc())
+                .transform(groupBy(ingredientGroup)
+                        .list(new QIngredientGroupInfo(ingredientGroup.id, ingredientGroup.name,
+                                ingredientGroup.selectCount.minCount, ingredientGroup.selectCount.maxCount,
+                                list(new QIngredientInfo(ingredient.id, ingredient.name, ingredient.price)))
+                        )
+                );
+        productDetailInfo.setIngredientGroups(ingredientGroups);
+
+        List<OptionGroupInfo> optionGroups = queryFactory
+                .from(productOptionGroup)
+                .leftJoin(productOptionGroup.optionGroup, optionGroup)
+                .leftJoin(optionInGroup).on(optionInGroup.optionGroup.eq(optionGroup))
+                .leftJoin(optionInGroup.option, option)
+                .where(productOptionGroup.product.id.eq(productId),
+                        productOptionGroup.displayInfo.isDisplay.isTrue(),
+                        optionInGroup.displayInfo.isDisplay.isTrue())
+                .orderBy(productOptionGroup.displayInfo.displayOrder.displayOrder.asc(),
+                        optionInGroup.displayInfo.displayOrder.displayOrder.asc())
+                .transform(groupBy(optionGroup)
+                        .list(new QOptionGroupInfo(optionGroup.id, optionGroup.name, optionGroup.maxSelectCount,
+                                list(new QOptionInfo(option.id, option.name, option.price)))));
+        productDetailInfo.setOptionGroups(optionGroups);
+
+        return productDetailInfo;
     }
 }
--- slow_delivery/src/test/java/be/shop/slow_delivery/product/infra/ProductQueryDaoTest.java
+++ slow_delivery/src/test/java/be/shop/slow_delivery/product/infra/ProductQueryDaoTest.java
@@ -8,8 +8,10 @@ import be.shop.slow_delivery.product.domain.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.persistence.EntityManager;
@@ -17,6 +19,7 @@ import javax.persistence.EntityManager;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import({JpaQueryFactoryConfig.class, ProductQueryDao.class})
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -26,7 +29,7 @@ class ProductQueryDaoTest {
     @Autowired
     private EntityManager em;
 
-    @Test
+    @Test @Rollback(value = false)
     void findProductDetailInfo() throws Exception{
         //given
         Product product = Product.builder()
@@ -37,14 +40,32 @@ class ProductQueryDaoTest {
                 .maxOrderQuantity(new Quantity(5))
                 .build();
         em.persist(product);
+        setIngredients(product);
+        setOptions(product);
+        em.flush();
+        em.clear();
 
-        IngredientGroup groupA = new IngredientGroup("groupA", new SelectCount(1, 1));
-        IngredientGroup groupB = new IngredientGroup("groupB", new SelectCount(1, 2));
-        em.persist(groupA);
-        em.persist(groupB);
+        //when
+        ProductDetailInfo productDetailInfo = productQueryDao.findProductDetailInfo(product.getId());
 
-        product.addIngredientGroup(groupA, 1);
-        product.addIngredientGroup(groupB, 2);
+        //then
+        assertThat(productDetailInfo.getProductId()).isEqualTo(product.getId());
+        assertThat(productDetailInfo.getName()).isEqualTo(product.getName());
+        assertThat(productDetailInfo.getIngredientGroups().size()).isEqualTo(2);
+        assertThat(productDetailInfo.getIngredientGroups().get(0).getIngredients().size()).isEqualTo(1);
+        assertThat(productDetailInfo.getIngredientGroups().get(1).getIngredients().size()).isEqualTo(2);
+        assertThat(productDetailInfo.getOptionGroups().size()).isEqualTo(1);
+        assertThat(productDetailInfo.getOptionGroups().get(0).getOptions().size()).isEqualTo(3);
+    }
+
+    private void setIngredients(Product product) {
+        IngredientGroup ingredientGroupA = new IngredientGroup("groupA", new SelectCount(1, 1));
+        IngredientGroup ingredientGroupB = new IngredientGroup("groupB", new SelectCount(1, 2));
+        em.persist(ingredientGroupA);
+        em.persist(ingredientGroupB);
+
+        product.addIngredientGroup(ingredientGroupA, 1);
+        product.addIngredientGroup(ingredientGroupB, 2);
 
         Ingredient ingredientA = Ingredient.builder()
                 .stockId(2L)
@@ -61,18 +82,39 @@ class ProductQueryDaoTest {
                 .name("ingredientC")
                 .price(new Money(3000))
                 .build();
+        Ingredient hidingIngredient = Ingredient.builder()
+                .stockId(4L)
+                .name("hidingIngredient")
+                .price(new Money(3000))
+                .build();
         em.persist(ingredientA);
         em.persist(ingredientB);
         em.persist(ingredientC);
+        em.persist(hidingIngredient);
 
-        groupA.addIngredient(ingredientA, 1);
-        groupB.addIngredient(ingredientB, 1);
-        groupB.addIngredient(ingredientC, 2);
+        ingredientGroupA.addIngredient(ingredientA, 1);
+        ingredientGroupA.addIngredient(hidingIngredient, 2);
+        ingredientGroupB.addIngredient(ingredientB, 1);
+        ingredientGroupB.addIngredient(ingredientC, 2);
 
-        OptionGroup group = new OptionGroup("groupB", new Quantity(10));
-        em.persist(group);
+        em.createQuery("update IngredientInGroup ig set ig.displayInfo.isDisplay = false where ig.ingredient.id =: igId")
+                .setParameter("igId", hidingIngredient.getId())
+                .executeUpdate();
+    }
 
-        em.persist(new ProductOptionGroup(product, group, 2));
+    private void setOptions(Product product) {
+        OptionGroup optionGroup = new OptionGroup("groupB", new Quantity(10));
+        em.persist(optionGroup);
+
+        OptionGroup hidingOptionGroup = new OptionGroup("groupB", new Quantity(10));
+        em.persist(hidingOptionGroup);
+
+        em.persist(new ProductOptionGroup(product, optionGroup, 1));
+        em.persist(new ProductOptionGroup(product, hidingOptionGroup, 2));
+
+        em.createQuery("update ProductOptionGroup pog set pog.displayInfo.isDisplay = false where pog.id =: pogId")
+                .setParameter("pogId", hidingOptionGroup.getId())
+                .executeUpdate();
 
         Option optionA = Option.builder()
                 .stockId(2L)
@@ -93,19 +135,8 @@ class ProductQueryDaoTest {
         em.persist(optionB);
         em.persist(optionC);
 
-        group.addOption(optionA, 1);
-        group.addOption(optionB, 2);
-        group.addOption(optionC, 3);
-
-        em.flush();
-        em.clear();
-
-        //when
-        ProductDetailInfo productDetailInfo = productQueryDao.findProductDetailInfo(product.getId());
-
-        //then
-        assertThat(productDetailInfo.getProductId()).isEqualTo(product.getId());
-        assertThat(productDetailInfo.getName()).isEqualTo(product.getName());
-        assertThat(productDetailInfo.getIngredientGroups().size()).isEqualTo(2);
+        optionGroup.addOption(optionA, 1);
+        optionGroup.addOption(optionB, 2);
+        optionGroup.addOption(optionC, 3);
     }
 }
